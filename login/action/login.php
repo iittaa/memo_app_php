8edee42f406bd7b5a8fc55516969f62ef8534e2c
板本　耕輔 <kosuke.723723@gmail.com>
2021-07-05 17:45:31 +0900

    ログアウト機能、ログイン時に最新投稿を表示する

--- login/action/login.php
+++ login/action/login.php
@@ -35,12 +35,27 @@ if ($stmt = $pdo->prepare("SELECT id, name, password FROM users WHERE email = :u
   $name = $user["name"];
   $id = $user["id"];
   $password = $user["password"];
-
+  
   if (password_verify($user_password, $password)) {
     $_SESSION["user"] = [
       "name" => $name,
       "id" => $id
     ];
+    
+    // 最新更新のメモの情報を保持する
+    if ($stmt = $pdo->prepare("SELECT id, title, content FROM memos WHERE user_id = :user_id ORDER BY updated_at DESC LIMIT 1")) {
+      $stmt->bindParam(":user_id", $id);
+      $stmt->execute();
+      $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      if ($result) {
+        $_SESSION["select_memo"] = [
+          "id" => $result["id"],
+          "title" => $result["title"],
+          "content" => $result["content"]
+        ];
+      }
+    }
     header("Location: ../../memo/index.php");
     exit;
   } else {
@@ -50,11 +65,6 @@ if ($stmt = $pdo->prepare("SELECT id, name, password FROM users WHERE email = :u
     header("Location: ../index.php");
     exit;
   }
-
-
-
-
 }
-
 
 ?>
